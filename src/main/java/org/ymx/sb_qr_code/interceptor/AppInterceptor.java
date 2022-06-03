d1442e541079568e520de7b59ad7980c1d06c99a
yanmx <1712229564@qq.com>
2022-06-03 09:04:16 +0800

    完成基本架构

--- src/main/java/org/ymx/sb_qr_code/interceptor/AppInterceptor.java
+++ src/main/java/org/ymx/sb_qr_code/interceptor/AppInterceptor.java
@@ -1,9 +1,32 @@
 package org.ymx.sb_qr_code.interceptor;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @desc: 拦截器
  * @author: YanMingXin
  * @create: 2022/6/3-8:49
  **/
-public class AppInterceptor {
+@Component
+public class AppInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
 }
